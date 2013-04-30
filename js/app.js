5796530dcf6806444c07c8c147e99cb316b81930
cbn <chadbnelson@gmail.com>
2013-04-29 22:41:12 -0400

    Working Color Map

--- js/app.js
+++ js/app.js
@@ -7,6 +7,6 @@ angular.module('myApp',[] ).
     $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: allStates});
     $routeProvider.when('/state/:state', {templateUrl: 'partials/partial2.html', controller: state});
     $routeProvider.when('/state/:state/county/:county', {templateUrl: 'partials/county.html', controller: county});
-    $routeProvider.when('/state/:state/county/:city', {templateUrl: 'partials/city.html', controller: city});
+    $routeProvider.when('/state/:state/city/:city', {templateUrl: 'partials/city.html', controller: city});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
--- js/controllers.js
+++ js/controllers.js
@@ -8,10 +8,40 @@ function myFunc(data) {
 
 function allStates($http, $scope) {
   $http.jsonp('http://api.dp.la/v2/items?sourceResource.spatial.country=United+States&page_size=0&facets=sourceResource.spatial.state&callback=JSON_CALLBACK&api_key=9da474273d98c8dc3dc567939e89f9f8').success(function(data) {
+        function getColor(d) {
+    return d > 10000 ? '#800026' :
+           d > 5000  ? '#BD0026' :
+           d > 2000  ? '#E31A1C' :
+           d > 1000  ? '#FC4E2A' :
+           d > 500   ? '#FD8D3C' :
+           d > 200   ? '#FEB24C' :
+           d > 100   ? '#FED976' :
+                      '#BADA55';
+}
+
+function style(feature) {
+    //console.log(feature);
+    return {
+      fillColor: getColor(feature.properties.count),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}    
+    $http.get('data/states.js').success(function(state_data) {
+    $scope.shapes = state_data;
     $scope.states = data;
-    $scope.obj = {};
-    data.facets['sourceResource.spatial.state'].terms.forEach(function(el) {$scope.obj[el.term] = el.count});
-  })
+    var obj = {};
+    data.facets['sourceResource.spatial.state'].terms.forEach(function(el) { obj[el.term] = el.count});
+    $scope.shapes.features.forEach(function(feature, index){$scope.shapes.features[index].properties.count = obj[feature.properties.Name]; console.log(feature.properties.Name); });
+    L.geoJson($scope.shapes, {style : style}).addTo(map);
+    })
+
+
+
+})
 }
 
 function state($http, $scope, $routeParams){
@@ -22,15 +52,16 @@ function state($http, $scope, $routeParams){
 }
 
 function county($http, $scope, $routeParams){
-  $http.jsonp('http://api.dp.la/v2/items?sourceResource.spatial.state=' + $routeParams.state + ' &sourceResource.spatial.county='+ $routeParams.county +'page_size=10&callback=JSON_CALLBACK&api_key=9da474273d98c8dc3dc567939e89f9f8').success(function(data) {
+  $http.jsonp('http://api.dp.la/v2/items?sourceResource.spatial.state=' + $routeParams.state + ' &sourceResource.spatial.county='+ $routeParams.county +'&page_size=10&callback=JSON_CALLBACK&api_key=9da474273d98c8dc3dc567939e89f9f8').success(function(data) {
     $scope.county = data;
     $scope.params = $routeParams;
   })
 }
 
 function city($http, $scope, $routeParams){
-  $http.jsonp('http://api.dp.la/v2/items?sourceResource.spatial.state=' + $routeParams.state + ' &sourceResource.spatial.county='+ $routeParams.city +'page_size=10&callback=JSON_CALLBACK&api_key=9da474273d98c8dc3dc567939e89f9f8').success(function(data) {
-    $scope.state = data;
+  $http.jsonp('http://api.dp.la/v2/items?sourceResource.spatial.state=' + $routeParams.state + ' &sourceResource.spatial.county='+ $routeParams.city +'&page_size=10&callback=JSON_CALLBACK&api_key=9da474273d98c8dc3dc567939e89f9f8').success(function(data) {
+    $scope.city = data;
+    $scope.params = $routeParams;
   })
 }
 
